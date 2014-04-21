26fac9435d1bacf4c84355165b8aa179fb354bd1
jesper <jesper.liljegren@gmail.com>
2014-04-21 12:58:11 +0200

    git plz

--- gulp/tasks/browserify.js
+++ gulp/tasks/browserify.js
@@ -1,7 +1,8 @@
 var browserify   = require('browserify');
 var gulp         = require('gulp');
-var handleErrors = require('../util/handleErrors');
 var livereload   = require('gulp-livereload');
+var concat       = require('gulp-concat-sourcemap');
+var handleErrors = require('../util/handleErrors');
 var source       = require('vinyl-source-stream');
 
 gulp.task('browserify', function(){
@@ -12,4 +13,12 @@ gulp.task('browserify', function(){
 		.pipe(source('app.js'))
 		.pipe(gulp.dest('./app_root/build/'))
 		.pipe(livereload());
+});
+
+// Third-party libraries
+gulp.task('libs', function() {
+	console.log("ok");
+	gulp.src(['./app_root/src/vendor/**/*.js'])
+	.pipe(concat('libs.js'))
+	.pipe(gulp.dest('./app_root/build'));
 });
